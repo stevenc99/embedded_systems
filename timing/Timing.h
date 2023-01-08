4f4ca7d75d25d3977626d59dd0a0c557566c7191
Neroxeles <69595189+Neroxeles@users.noreply.github.com>
2023-01-08 14:09:59 +0100

    Update Timing.h

--- timing/Timing.h
+++ timing/Timing.h
@@ -3,6 +3,12 @@
 
 #include <Arduino.h>
 
+// Starte den Schweißvorgang
+// return true when successful
+bool startWelding(uint8_t pwm, uint8_t delay);
 
+// Stoppe den Schweißvorgang
+// Stelle den sicheren Zustand wieder her
+void stopWelding();
 
-#endif // TIMING_H
+#endif /* TIMING_H */
