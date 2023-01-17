0375097ac13f96f96d0e3885723c151e654328b6
Steven Chamberlain <steven_william.chamberlain@stud.hs-merseburg.de>
2023-01-17 20:11:38 +0100

    Config: Remove redundant methods

--- src/config/Config.h
+++ src/config/Config.h
@@ -14,15 +14,6 @@ class Config {
         void Save();
         void Load();
 
-        void SetPWM(uint8_t p) { this->pwm = p; };
-        uint8_t GetPWM() { return this->pwm; };
-
-        void SetGasFollowUpTime(uint8_t f) { this->gas_follow_up_time = f; };
-        uint8_t GetGasFollowUpTime() { return this->gas_follow_up_time; };
-
-        void SetGasLeadTime(uint8_t l) { this->gas_lead_time = l; };
-        uint8_t GetGasLeadTime() { return this->gas_lead_time; };
-
         uint8_t getValue(uint8_t menuPointerPosition) {
             if (menuPointerPosition == 1) {
                 return this->pwm;
