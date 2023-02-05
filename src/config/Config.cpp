e91affff462841170aadf4b4c98ecd01cab192d8
Neroxeles <69595189+Neroxeles@users.noreply.github.com>
2023-02-05 11:55:40 +0100

    Update Config.cpp

--- src/config/Config.cpp
+++ src/config/Config.cpp
@@ -4,23 +4,10 @@
 void Config::Load() {
   int address = 0;
   EEPROM.get(address, pwm);
-  Serial.print("address = ");
-  Serial.println(address);
-  Serial.print("PWM = ");
-  Serial.println(pwm);
   address += sizeof(uint8_t);
   EEPROM.get(address, gas_follow_up_time);
-  Serial.print("address = ");
-  Serial.println(address);
-  Serial.print("gas_follow_up_time = ");
-  Serial.println(gas_follow_up_time);
   address += sizeof(uint8_t);
   EEPROM.get(address, gas_lead_time);
-  Serial.print("address = ");
-  Serial.println(address);
-  Serial.print("gas_lead_time = ");
-  Serial.println(gas_lead_time);
-  Serial.println("Load");
 }
 
 // Speichere Konfigurationen in den EEPROM
@@ -28,21 +15,8 @@ void Config::Save() {
   Serial.println("Save");
   int address = 0;
   EEPROM.update(address, pwm);
-  Serial.print("address = ");
-  Serial.println(address);
-  Serial.print("PWM = ");
-  Serial.println(pwm);
   address += sizeof(uint8_t);
   EEPROM.update(address, gas_follow_up_time);
-  Serial.print("address = ");
-  Serial.println(address);
-  Serial.print("gas_follow_up_time = ");
-  Serial.println(gas_follow_up_time);
   address += sizeof(uint8_t);
   EEPROM.update(address, gas_lead_time);
-  Serial.print("address = ");
-  Serial.println(address);
-  Serial.print("gas_lead_time = ");
-  Serial.println(gas_lead_time);
-  Serial.println("Save");
 }
